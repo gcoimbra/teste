fe0bd17f7f7e045de80fba74a03098467f40315a
Letícia Silva <95985205+lleticiasilvaa@users.noreply.github.com>
2022-04-07 22:17:00 -0300

    Código Comentado

--- URI-1754.py
+++ URI-1754.py
@@ -1,10 +1,12 @@
+#URI - 1754 - A Sala do Tempo
 import math
 T = int(input())
 for i in range (T):
   X, Y = input().split()
-  X = int(X) 
-  Y = int(Y) 
+  X = int(X) #tempo necessario para completar o treinamento
+  Y = int(Y) #tempo do trajeto de Super Buu
   K = 2 # X<Y 
-  if (X>Y):
+  if (X>Y): # X <= Y*K
+    # K >= X/Y : K = menor inteiro maior ou igula (X/Y) = função teto
     K = math.ceil(X/Y)
-  print(K) 
+  print(K) # quantidade de segundos que se passa dentro da sala durante 1 segundo fora da mesma
